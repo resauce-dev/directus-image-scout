06272c91141a1fd6e5a5a72100a9a1d97c8c119d
Shea Lavington <shealavington@gmail.com>
2020-09-17 13:34:05 +0100

    Initial Setup based on Docs

---
+++ src/interface.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>My Custom Interface</div>
+</template>
+
+<script>
+export default {}
+
+/**
+
+The props you can use in an interface are:
+
+value - Current value of the field
+width - The current width within the form, one of half, half-left, half-right, full, fill
+type - The type of the field
+collection - The current collection the field is in
+field - The key of the field
+primary-key - The primary key of the current item
+
+ */
+</script>
