54095f382dacbd90f85977b1c6cb2a5d362e1235
Shea Lavington <shealavington@gmail.com>
2020-09-17 15:29:15 +0100

    Update meta

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import InterfaceComponent from './interface.vue';
 
 export default {
     id: 'unsplash',
-    name: 'Unsplash',
-    description: 'Select images from the Unsplash image library',
+    name: 'Unsplash Image',
+    description: 'Select an image from the Unsplash image library',
     icon: 'image_search',
     component: InterfaceComponent,
+    relationship: 'm2o',
     types: ['uuid'],
+    recommendedDisplays: ['image']
 }
