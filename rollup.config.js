47d116dd7df9258c899b0885f34c3c66ada3ba87
Shea Lavington <shealavington@gmail.com>
2021-01-14 19:40:40 +0000

    update build process

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,25 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
 
-export default {
-    input: 'src/index.js',
+export default [
+  {
+    input: 'src/interface/index.js',
     output: {
-        format: 'es',
-        file: 'dist/index.js'
+      format: 'es',
+      file: 'dist/extensions/interfaces/resauce-image-scout/index.js'
     },
     plugins: [
-        terser(),
-        resolve(),
-        commonjs(),
-        vue()
+      terser(),
+      resolve(),
+      commonjs(),
+      vue()
     ]
-}
+  },
+  {
+    input: 'src/endpoints/index.js',
+    output: {
+      format: 'es',
+      file: 'dist/extensions/endpoints/resauce-image-scout/index.js',
+    }
+  }
+]
