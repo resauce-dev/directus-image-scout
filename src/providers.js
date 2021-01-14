589a1379a610ede3ea4d8e1fd9ee9aa2c3a649ed
Shea Lavington <shealavington@gmail.com>
2021-01-14 18:24:09 +0000

    Remove old providers

--- src/providers.js
+++
@@ -1,6 +0,0 @@
-export default [
-  { text: 'Unsplash', value: 'unsplash', url: 'https://unsplash.com' },
-  { text: 'Pixabay', value: 'pixabay', url: 'https://pixabay.com' },
-  { text: 'Pexels', value: 'pexels', url: 'https://www.pexels.com' },
-  { text: 'Giphy', value: 'giphy', url: 'https://giphy.com' },
-]
