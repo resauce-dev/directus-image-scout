754a196bfed259dcefd1549d681628763c8cc7ed
Shea Lavington <shealavington@gmail.com>
2021-01-15 12:37:05 +0000

    Update build process

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { terser } from 'rollup-plugin-terser'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
+import copy from 'rollup-plugin-copy'
 
 export default [
   {
@@ -22,6 +23,16 @@ export default [
     output: {
       format: 'es',
       file: 'dist/extensions/endpoints/resauce-image-scout/index.js',
-    }
+    },
+    plugins: [
+      copy({
+        targets: [
+          { 
+            src: 'src/endpoints/*', 
+            dest: 'dist/extensions/endpoints/resauce-image-scout'
+          }
+        ]
+      })
+    ]
   }
 ]
