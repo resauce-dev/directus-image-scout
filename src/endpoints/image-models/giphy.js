e3878614f6bce8f01c4b7ea5c044272381c01ba1
Shea Lavington <shealavington@gmail.com>
2021-02-09 17:36:38 +0000

    convert model to use 'this'

--- src/endpoints/image-models/giphy.js
+++ src/endpoints/image-models/giphy.js
@@ -6,12 +6,12 @@ module.exports = class GiphyImageModel extends ImageModel {
 
     image.title = image.title.replace(/\s?GIF/g, '')
 
-    model.setAttribution(image.title, image.url)
-    model.setPreviewUrl(image.images.original.url)
+    this.setAttribution(image.title, image.url)
+    this.setPreviewUrl(image.images.original.url)
 
-    model.setTitle(image.title)
-    model.setFileName(`${image.id}.gif`)
-    if(image.tags) { model.setTags(image.tags) }
-    model.setSize(image.images.fixed_width.width, image.images.fixed_width.height)
+    this.setTitle(image.title)
+    this.setFileName(`${image.id}.gif`)
+    if(image.tags) { this.setTags(image.tags) }
+    this.setSize(image.images.fixed_width.width, image.images.fixed_width.height)
   }
 }
