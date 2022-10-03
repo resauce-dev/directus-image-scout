816fa05d8ab5d42f2465d240b31a6bc3ab6ae7a9
Shea Lavington <shealavington@gmail.com>
2022-10-03 21:22:41 +0100

    Formatting

--- src/endpoints/classes/Provider.js
+++ src/endpoints/classes/Provider.js
@@ -54,38 +54,38 @@ module.exports = class Provider {
 	 * @param {*} data
 	 */
 	formatResults(data) {
-    const ImageModel = require(__dirname + `/../image-models/${this.key.toLowerCase()}`)
-    return data.map(img => new ImageModel(img))
-  }
-  /**
-   * Download an image URL to Directus
-   * 
-   * @param {Object} req
-   */
-  async downloadImage(req) {
-    const postUrl = `${req.getApiUrl()}/files/import?access_token=${req.getBody().access_token}`
-    const postBody = {
-      url: req.getBody().image.url_download, 
-      data: this.formatImageDataForImport(req.getBody().image)
-    }
-    const { data } = await axios.post(postUrl, postBody)
-    return data
-  }
+		const ImageModel = require(__dirname + `/../image-models/${this.key.toLowerCase()}`)
+		return data.map(img => new ImageModel(img))
+	}
+	/**
+	 * Download an image URL to Directus
+	 * 
+	 * @param {Object} req
+	 */
+	async downloadImage(req) {
+		const postUrl = `${req.getApiUrl()}/files/import?access_token=${req.getBody().access_token}`
+		const postBody = {
+			url: req.getBody().image.url_download, 
+			data: this.formatImageDataForImport(req.getBody().image)
+		}
+		const { data } = await axios.post(postUrl, postBody)
+		return data
+	}
 	/**
 	 * Return the data needed to provide to Directus import
-   * 
-   * @param {*} image
+	 * 
+	 * @param {*} image
 	 */
 	formatImageDataForImport(image) {
-    const data = {}
+		const data = {}
 
-    // Conditionally add if set to avoid blank data going in
-    if(image.title) data.title = image.title
-    if(image.description) data.description = image.description
-    if(image.location) data.location = image.location
-    if(image.tags) data.tags = JSON.stringify(image.tags)
-    if(image.filename_download) data.filename_download = image.filename_download
+		// Conditionally add if set to avoid blank data going in
+		if(image.title) data.title = image.title
+		if(image.description) data.description = image.description
+		if(image.location) data.location = image.location
+		if(image.tags) data.tags = JSON.stringify(image.tags)
+		if(image.filename_download) data.filename_download = image.filename_download
 
-    return data
+		return data
 	}
 }
